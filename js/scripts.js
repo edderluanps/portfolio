2eae2076ce83d4b5589c0985b6cf3009fd988795
Edder Luan <eluann100@gmail.com>
2022-12-19 23:46:10 -0300

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    $(".sidenav").toggle();
+    $(window).scroll(function () {
+        if (this.scrollY > 20) {
+            $('.navbar').addClass("sticky");
+
+        } else {
+            $('.navbar').removeClass("sticky");
+
+        } if (this.scrollY > 600) {
+            $('.btn-scroll-up').addClass("show");
+
+        } else {
+            $('.btn-scroll-up').removeClass("show");
+
+        }
+    });
+
+    $('.btn-scroll-up').click(function () {
+        $('html').animate({ scrollTop: 0 });
+    });
+
+    $(".menu-icon").click(function(){
+        $(".sidenav").toggle();
+    });
+
+    $(".sidenav a").click(function(){
+        $(".sidenav").toggle();
+    });
+
+});
